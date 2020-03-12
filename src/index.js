7becede74d09b3df09ac63036479439aa97f51db
Vitali Matusevich <vimatu@gmail.com>
2020-03-12 14:49:11 +0300

    feat:  const readNum = {

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 module.exports = function toReadable(number) {
     const readNum = {
-        ones: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-        tens: ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+        ones: ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        tens: ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
         hundred: 'hundred',
     }
-
+    if (number === 0) return 'zero';
     const toNumbTens = (num) => {
-        if (num < 20) return readNum.ones[number];
-        const numString = number.toString();
-        return `${readNum.tens[numString[0]]} ${readNum.ones[numString[1]]}`;
+        if (num < 20) return readNum.ones[num];
+        const numString = num.toString();
+        return `${readNum.tens[numString[0]]} ${readNum.ones[numString[1]]}`.trim();
     }
 
     if (number < 100) {
         return toNumbTens(number);
     }
-    
+
     const numString = number.toString();
     if (numString.length === 3) {
-        return `${readNum.ones[numString[0]]} ${readNum.hundred} ${toNumbTens(+numString.slice(-2))}`;
+        const tens = toNumbTens(number % 100);
+        return `${readNum.ones[numString[0]]} ${readNum.hundred} ${tens}`.trim();
     }
 }
